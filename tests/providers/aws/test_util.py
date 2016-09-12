b4958efe10d47af260ca918bf1ac8f54945b8391
Ovais Tariq <ovaistariq@gmail.com>
2016-09-11 23:07:03 -0700

    Add tests for util

---
+++ tests/providers/aws/test_util.py
@@ -0,0 +1,17 @@
+import pytest
+from twindb_infrastructure.providers.aws import util
+
+
+def test__region_name_is_validated():
+    assert util.is_region('incorrect-region') == False
+    assert util.is_region('us-east-1')
+
+
+def test__zone_name_is_validated():
+    assert util.is_valid_zone('incorrect-zone') == False
+    assert util.is_valid_zone('us-east-1a')
+
+
+def test__get_region_returns_valid_region_name():
+    assert util.get_region('invalid-zone') is None
+    assert util.get_region('us-east-1a') == 'us-east-1'
