c4b38a1a257deac111059c8013a2ac5920f511ef
Aleksandr Kuzminsky <aleks@twindb.com>
2017-01-26 20:49:05 -0800

    Bump version: 0.1.3 → 1.0.0

--- twindb_infrastructure/__init__.py
+++ twindb_infrastructure/__init__.py
@@ -6,7 +6,7 @@ from logutils.colorize import ColorizingStreamHandler
 
 __author__ = 'TwinDB Development Team'
 __email__ = 'dev@twindb.com'
-__version__ = '0.1.3'
+__version__ = '1.0.0'
 
 log = logging.getLogger(__name__)
 
